15f14d115c532498614a47633ff2b54e25360055
ruanyong <ruanyong@windows10.microdone.cn>
2018-12-03 22:13:54 +0800

    修改life_bat-12.3

--- Datas/battery life test/life_bat.m
+++ Datas/battery life test/life_bat.m
@@ -1,71 +1,42 @@
-Q0 = 0.063765;
-I_c = 0.06;
-I_d = 0.12;
-k = 2;
-Q_c = zeros(10*k,1);
-Q_d = zeros(10*k,1);
-Q_ck = zeros(10,k);
-Q_dk = zeros(10,k);
-Qloss_ck = zeros(10,k);
-Qloss_dk = zeros(10,k);
-dQloss = zeros(1,k);
-for j=1:k
-    num_j = xlsread(['E:\qnj\科研\03复合能量收集\实验\锂电池测试\寿命测试\寿命测试' num2str(j) '.csv']);
-    if j==1
-        num = num_j;
+fileFolder=fullfile('E:\qnj\科研\03复合能量收集\实验\锂电池测试\寿命测试\11mAh\bat1');
+dirOutput=dir(fullfile(fileFolder,'*.csv'));
+fileNames={dirOutput.name}';
+fileNums = length(fileNames);
+
+tv = [];
+Qd1 = [];
+Qd2 = [];
+Ah1 = [0];
+Ah2 = [0];
+I_c1 = 0.01;
+
+for j=1:fileNums
+    csvname = fileNames{j};
+    c_rate = str2double(csvname(1));
+    tv_j = xlsread(['E:\qnj\科研\03复合能量收集\实验\锂电池测试\寿命测试\11mAh\bat1\' csvname]);
+    v = tv_j(:,2);
+    [~,imax] = findpeaks(v,'MinPeakProminence',0.6);
+    [~,imin] = findpeaks(-v,'MinPeakProminence',1);
+    index = [1;imax;imin;length(v)];
+    index = sort(index);
+    N = floor(length(index)/2);
+    if c_rate == 2
+        Qd_i = I_c1*c_rate*(index(2*N)-index(2*N-1))/3600/10;
+        Qd1 = [Qd1;Qd_i];
+        Ah_i = 0;
+        for i=1:N
+            Ah_i = Ah_i+2*I_c1*c_rate*(index(2*i)-index(2*i-1))/3600/10;
+        end
+        Ah1 = [Ah1;Ah_i];
     else
-        num_j(:,1) = num_j(:,1)+num(end,1);
-        num = [num;num_j];
+        for i=1:N
+            Qd_i = I_c1*c_rate*(index(2*i)-index(2*i-1))/3600/10;
+            Qd2 = [Qd2;Qd_i];
+            Ah_i = Qd_i*2;
+            Ah2 = [Ah2;Ah_i];
+        end
     end
 end
-t = num(:,1);
-v = num(:,2);
-plot(t,v)
-[vmax,imax] = findpeaks(v,'MinPeakProminence',0.6);
-[vmin,imin] = findpeaks(-v,'MinPeakProminence',1);
-index = [1;imax;imin;length(v)];
-index = sort(index);
-n = floor(length(index)/2);
-for i=1:n
-    Q_d(i) = I_d*(index(2*i)-index(2*i-1))/3600;
-    Q_c(i) = I_c*(index(2*i+1)-index(2*i))/3600;
-end
-for j=1:k
-    Q_ck(:,j) = Q_c((-9:0)+10*j);
-    Qloss_ck(:,j) = (Q_c(1)-Q_ck(:,j))/Q_c(1);
-    Q_dk(:,j) = Q_d((-9:0)+10*j);
-    Qloss_dk(:,j) = (Q_d(1)-Q_dk(:,j))/Q_d(1);
-    Qdata = [Q_ck; Qloss_ck; Q_dk; Qloss_dk];
-    if  j~=1
-        dQloss(j) = (Q_d((j-1)*10)-Q_d(j*10))/Q_d(1);
-    else
-        dQloss(j) = (Q_d(1)-Q_d(10))/Q_d(1);
-    end
-end
+dQloss1 = (Qd1(1)-Qd1)/Qd1(1);
+dQloss2 = (Qd2(1)-Qd2)/Qd2(1);
 
-para0 = [0.824,0.0032,-1516];
-para = lsqcurvefit(@compute,para0,Qdata,dQloss);
-
-function dQloss = compute(para,Qdata)
-I_c = 0.06;
-I_d = 0.12;
-% input parameter
-Q_c = Qdata(1:10,:);
-Qloss_c = Qdata(11:20,:);
-Q_d = Qdata(21:30,:);
-Qloss_d = Qdata(31:40,:);
-% baterry parameter
-Q0_c = Q_c(1,1);
-Q0_d = Q_d(1,1);
-R  = 8.314; % Gas constant (J/(mol K));
-T  = 273.15+40; % Absolute temperature (K);
-Ea = 15162; % Activation energy (J);
-dQloss = 0;
-z = para(1);
-A0 = para(2);
-B = para(3);
-for i=1:10
-    dQloss = dQloss+Q_d(i,:)*z*A0.^(1/z)*exp(-(Ea+B*I_d/Q0_d)/(z*R*T)).*Qloss_d(i,:)^((z-1)/z);
-    dQloss = dQloss+Q_c(i,:)*z*A0.^(1/z)*exp(-(Ea+B*I_c/Q0_c)/(z*R*T)).*Qloss_c(i,:)^((z-1)/z);
-end
-end
