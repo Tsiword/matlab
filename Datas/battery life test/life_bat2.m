94daf89d9c9c53f3b416d3ecbd0032b80a59a0e3
ruanyong <ruanyong@windows10.microdone.cn>
2018-12-19 09:47:02 +0800

    分开拟合求参数

--- Datas/battery life test/life_bat2.m
+++ Datas/battery life test/life_bat2.m
@@ -1,33 +1,40 @@
-Q = xlsread('E:\qnj\科研\03复合能量收集\实验\锂电池测试\寿命测试\广州\70度.xlsx');
-Q = Q(:,2);
-dQloss = (Q(2:end-1)-Q(3:end))/Q(1);
-Qloss = 1-Q(2:end)/Q(1);
-para0 = [0.824,0.0032,-1516,15162];
-para = lsqcurvefit(@compute,para0,Qloss(1:end-1),dQloss);
-Qloss_s = Qloss;
-for i=1:length(Qloss)-1
-    dQloss_s = compute(para,Qloss_s(i));
-    Qloss_s(i+1) = Qloss_s(i)+dQloss_s;
+data = xlsread('E:\qnj\科研\03复合能量收集\实验\锂电池测试\寿命测试\11mAh\bat1\20D\数据整理3.xlsx');
+dQloss = data(:,4);
+Qloss = data(:,1:3);
+Ah = data(:,5);
+C_Rate = Qloss(:,1);
+Qloss1 = Qloss(C_Rate==1,:);
+Qloss2 = Qloss(C_Rate==2,:);
+Qloss3 = Qloss(C_Rate==3,:);
+dQloss1 = dQloss(C_Rate==1);
+dQloss2 = dQloss(C_Rate==2);
+dQloss3 = dQloss(C_Rate==3);
+para0 = [0.45,1.4];
+para1 = lsqcurvefit(@compute,para0,Qloss1,dQloss1);
+para2 = lsqcurvefit(@compute,para0,Qloss2,dQloss2);
+para3 = lsqcurvefit(@compute,para0,Qloss3,dQloss3);
+Qloss1_s = Qloss1;
+Qloss2_s = Qloss2;
+Qloss3_s = Qloss3;
+for i=1:length(Qloss1)-1
+    dQloss1_s = compute(para1,Qloss1(i,:));
+    Qloss1_s(i+1,2) = Qloss1_s(i,2)+dQloss1_s;
 end
-figure,plot(Qloss),hold on
-plot(Qloss_s)
+for i=1:length(Qloss2)-1
+    dQloss2_s = compute(para2,Qloss2(i,:));
+    Qloss2_s(i+1,2) = Qloss2_s(i,2)+dQloss1_s;
+end
+for i=1:length(Qloss3)-1
+    dQloss3_s = compute(para3,Qloss3(i,:));
+    Qloss3_s(i+1,2) = Qloss3_s(i,2)+dQloss3_s;
+end
+Qloss_s = [Qloss2_s;Qloss1_s;Qloss3_s];
+figure,plot(Ah,Qloss(:,2),'*'),hold on
+plot(Ah,Qloss_s(:,2),'o')
+legend('measured','modeled')
 
 function dQloss = compute(para,Qloss)
-% charge and discharge parameter
-I_c = 0.06;
-I_d = 0.12;
-Q0 = 61.72/1000;
-C_c = I_c/Q0;
-C_d = I_d/Q0;
-% baterry parameter
-R  = 8.314; % Gas constant (J/(mol K));
-T  = 273.15+70; % Absolute temperature (K);
-% Ea = 15162; % Activation energy (J);
 z = para(1);
-A0 = para(2);
-B = para(3);
-Ea = para(4);
-dQloss = Q0*(1-Qloss)*z*A0.^(1/z)*exp(-(Ea+B*C_c)/(z*R*T)).*Qloss.^((z-1)/z);
-Qloss = Qloss+dQloss;
-dQloss = dQloss+Q0*(1-Qloss)*z*A0.^(1/z)*exp(-(Ea+B*C_d)/(z*R*T)).*Qloss.^((z-1)/z);
+b = para(2);
+dQloss = z*exp(b/z)*Qloss(:,2).^(1-1/z).*Qloss(:,3);
 end
